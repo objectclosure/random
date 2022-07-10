b6d5cc45d81f27ffd73a0d81a9e8d93075575ffd
objectclosure <objectoftypeclosure@gmail.com>
2022-07-10 17:39:44 +0300

    more data.table

--- data-table.R
+++ data-table.R
@@ -1,3 +1,5 @@
+library(data.table)
+
 input <- if (file.exists("flights14.csv")) {
   "flights14.csv"
 } else {
@@ -33,3 +35,62 @@ flights[, .(arr_delay, dep_delay)]
 # Select both arr_delay and dep_delay columns and rename them to delay_arr 
 # and delay_dep:
 flights[, .(delay_arr = arr_delay, delay_dep = dep_delay)]
+
+# How many trips have had total delay < 0?
+flights[, sum((dep_delay + arr_delay) < 0)]
+
+# Calculate the average arrival and departure delay for all flights with “JFK” 
+# as the origin airport in the month of June.
+flights[
+  origin == "JFK" & month == 6L, 
+  list(
+    arr_av = mean(arr_delay), 
+    dep_av = mean(dep_delay)
+  )
+]
+
+# How many trips have been made in 2014 from “JFK” airport in the month of June?
+flights[origin == "JFK" & month == 6L & year == 2014L, .N]
+
+# But 2014 is the only year:
+flights[, unique(year)]
+
+# so we can just do:
+flights[origin == "JFK" & month == 6L, .N]
+
+
+# Select both arr_delay and dep_delay columns the data.frame way.
+flights[, c("arr_delay", "dep_delay")]
+
+# Select columns named in a variable using the .. prefix
+select_cols <- c("arr_delay", "dep_delay")
+flights[, ..select_cols]
+
+# The `..` is analogous the unix terminal where it refers to one level up.
+# Here it tells data.table to look one environment up
+
+# Select columns named in a variable using with = FALSE
+flights[, select_cols, with = FALSE]
+
+# `with` is analogous to base R's `with()`
+
+# We can also deselect columns using - or !:
+flights[, -c("arr_delay", "dep_delay")]
+flights[, !c("arr_delay", "dep_delay")]
+
+# From v1.9.5+, we can also select by specifying start and end column names, 
+# e.g., year:day to select the first three columns.
+flights[, year:day]
+flights[, -(year:day)]
+flights[, day:year]
+
+# How can we get the number of trips corresponding to each origin airport?
+flights[, .N, by = "origin"]
+flights[, .(no_of_trips = .N), by = "origin"]
+
+# How can we calculate the number of trips for each origin airport for carrier 
+# code "AA"?
+flights[carrier == "AA", .N, by = origin]
+
+# How can we get the total number of trips for each origin, dest pair for 
+# carrier code "AA"?
